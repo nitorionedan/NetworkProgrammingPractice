ba55e97bcf03249e52ede123e9825a6f30676254
nitorionedan <b1444263@planet.kanazawa-it.ac.jp>
2016-08-08 18:27:28 +0900

    creating the socket

--- main.cpp
+++ main.cpp
@@ -1,46 +1,75 @@
+/*
+@brief		ネットワークプログラミングの練習
+@reference	http://www.geekpage.jp/programming/winsock/
+*/
+
+
 #include <iostream>
 #include <WinSock2.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
 
-WSADATA g_wsaData;
-
-
-void Initialize(WSADATA wsaData);
+int Initialize(WSADATA* wsaData = nullptr);
+int CreateSocket(SOCKET* sock = nullptr);
 void Finalize();
 
-void Initialize(WSADATA wsaData) 
+
+int Initialize(WSADATA* wsaData) 
 {
-	g_wsaData = wsaData;
+	if (wsaData == nullptr)	return -1;
+
 	int err = 0;
 
-	err = WSAStartup(MAKEWORD(2, 0), &wsaData);
+	err = WSAStartup(MAKEWORD(2, 0), wsaData);
 
-	if (err == 0)	return;
+	if (err == 0)	return 0;
 
 	switch (err)
 	{
+	// ネットワークサブシステムがネットワークへの接続を準備できない
 	case WSASYSNOTREADY:
 		std::cout << "WSASYSNOTREADY\n";
 		break;
 
+	// 要求されたwinsockのバージョンがサポートされていない
 	case WSAVERNOTSUPPORTED:
 		std::cout << "WSAVERNOTSUPPORTED\n";
 		break;
 
+	// ブロッキング操作の実行中であるか、 
+	// またはサービスプロバイダがコールバック関数を処理している
 	case WSAEINPROGRESS:
 		std::cout << "WSAEINPROGRESS\n";
 		break;
-
+	
+	// winsockが処理できる最大プロセス数に達した
 	case WSAEPROCLIM:
 		std::cout << "WSAERRORCLIM\n";
 		break;
 
+	// 第二引数であるlpWSAData は有効なポインタではない
 	case WSAEFAULT:
 		std::cout << "WSAEFAULT\n";
 		break;
 	}
+
+	return -1;
+}
+
+
+int CreateSocket(SOCKET* sock)
+{
+	if (sock == nullptr)	return -1;
+
+	*sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (*sock == INVALID_SOCKET)
+	{
+		std::cout << "error : " << WSAGetLastError() << std::endl;
+		return -1;
+	}
+
+	return 0;
 }
 
 
@@ -50,11 +79,20 @@ void Finalize()
 }
 
 
+/*---------------------------------------
+Main
+---------------------------------------*/
 int main(int argc, char* argv[]) 
 {
+	/* initialization */
 	WSADATA wsaData;
-	Initialize(wsaData);
+	if(Initialize(&wsaData) != 0)	return -1;
 
+	/* creating the socket */
+	SOCKET sock;
+	if (CreateSocket(&sock) != 0)	return -1;
+
+	/* finish */
 	Finalize();
 	return 0;
 }
